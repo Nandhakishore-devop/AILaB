daec112aac8fc05126b23ddf365f5fb750bbd0d7
Nnadhakishore <nandhakishorebakkiyaraj@gmail.com>
2025-11-20 15:30:50 +0530

    'commited'

--- file1.py
+++ file1.py
@@ -1,9 +1,21 @@
 from sys import argv
 script, filename = argv
-txt = open(filename)
-print(f"Here's your file {filename}")
-print(txt.read())
-print("TYpe the file name again :")
-file_again=input("> ")
-txt_again=open(file_again)
-print(txt_again.read())
+
+try:
+    txt = open(filename)
+    print(f"Here's your file {filename}")
+    print(txt.read())
+    txt.close()
+except FileNotFoundError:
+    print(f"Error: File '{filename}' not found")
+    exit(1)
+
+print("Type the file name again :")
+filename_again = input("> ")
+
+try:
+    txt_again = open(filename_again)
+    print(txt_again.read())
+    txt_again.close()
+except FileNotFoundError:
+    print(f"Error: File '{filename_again}' not found")
